a5fd609c75ff83c506abebaee457c20fcfbbbe96
jeswin <jeswinpauljacob@gmail.com>
2019-08-01 19:55:10 +0530

    this is my second demo project

---
+++ onlinedemo2/online/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from online import views
+app_name="online"
+
+urlpatterns = [
+    path('',views.productlistview.as_view(),name='list'),
+    path('<int:pk>/',views.productdetailsview.as_view(),name='detail'),
+]
